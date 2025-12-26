3458dd86592786d9fe20fa71e884c32035b690d4
Madhav Banjade <madhavbnzd57@gmail.com>
2025-12-26 19:50:28 +0545

    fix: generate prisma client on deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 //should be aware with the import of prismaclient otherwise. it through error
 
 const globalForPrisma = global as unknown as {
